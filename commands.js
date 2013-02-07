8576934292c8c366cb6498e8473a4a8ae0d05139
boxmein <johannes12321@windowslive.com>
2013-02-08 00:08:46 +0200

    Re-added quote function, rewriting crypt functions

--- commands.js
+++ commands.js
@@ -1,30 +1,49 @@
 
 var output = require("./output.js");
 var config = require("./config.js");
-var crypto = require("./cryptography.js");
+
+// Warning! The commands below need ../ for core bot modules! 
+var crypto = require("./cmdsubmodules/cryptography.js");
+var quotes = require("./cmdsubmodules/quotes.js");
 var commands = {
   verify: function() { // Is called on connecting. Return true. Use for initialization.
     cmdpr.reloadList();
-    return true
+
+    return (!!(crypto) && !!(quotes));
   },
   setIRC: function(ircc) { commands.irc = ircc; },
   prefix: "#",
+  quote: function(ircdata) {
+    var index = Math.floor(Math.random() * 10) % quotes.q.length;
+    var current = quotes.q[index];
+    output.log("commands.quote", "Quote index:" + current);
+    output.log("commands.quote", "Sending quote: " + current);
+    cmdpr._respond(ircdata, current);
+  },
   crypt: function(ircdata) {
-    if (ircdata.args.length < 3)
-    {
-      // Send the help text
-      ircdata.args[0] = "crypt";
-      commands.help(ircdata);
-      return;
+    var subcommand = ircdata.args.shift(),
+    key = null,
+    plaintext = "",
+    reverse = false;
+    // Caesar crypt needs a shift counter
+    if (subcommand == "caesar") {
+        try { key = parseInt(ircdata.args.shift()); }
+        catch (err) { output.err("Key " + key + " was not parsable!"); }
+        plaintext = ircdata.args.join(" ");
+        crypto.caesar(key, plaintext);
     }
-    var subcommand = ircdata.args[0];
-    if (typeof crypto[subcommand] == "undefined") {
-      output.err("commands.crypt", "No such crypt subcommand: " + subcommand);
-      cmdpr._respond(ircdata, "No such crypto method: " + subcommand);
+    else if(subcommand == "rot13") {
+        plaintext = ircdata.args.join(" ");
+        crypto.rot13(plaintext);
     }
-    output.log("commands.crypt", "found crypto subcommand: " + subcommand);
-    output.log("commands.crypt", "calling with key: " + ircdata.args[1] + " & plain: " + ircdata.args[2]);
-    cmdpr._respond(ircdata, crypto[subcommand](ircdata.args[1], ircdata.args[2]));
+    else if(subcommand == "otp") {
+        return;
+        key = ircdata.args.shift();
+        if (key.indexOf("-") == 0)
+          reverse = true;
+        plaintext = ircdata.args.join(" ");
+        crypto.otp(key, plaintext, reverse);
+    }
   },
   eat: function(ircdata) { // eat <user> | eat - eats the user or the sender
     output.log("commands.eat", "Eating an user.");
@@ -54,7 +73,7 @@ var commands = {
       cmdpr._respond(ircdata, commands.cmdlist[requested]);
     }
     else 
-      output.log("commands.help", "command was either not found or empty string");
+      output.err("commands.help", "command was either not found or empty string");
   },
   version: function(ircdata) { // version - id string
     cmdpr._respond(ircdata, "boxmein's bot in node.js : https://gist.github.com/4644761");
@@ -63,18 +82,17 @@ var commands = {
     cmdpr._respond(ircdata, cmdpr._isowner(ircdata));
   },
   js: function(ircdata) { // js <code> - makes javascript
-    output.log("commands.js", "JS METHOD CALL STARTED");
     if (cmdpr._isowner(ircdata)) {
-      output.log("commands.js", "Sender is the owner! Executing...");
-      output.log("commands.js", "JS: " + ircdata.args.join(" "));
+      var js = ircdata.args.join(" ");
+      output.log("commands.js", "JS: " + js);
       var response = "";
       try {
-        response = "[return]: " + eval("(function() {" + ircdata.args.join(" ") + "})();");
+        response = "[return]: " + eval("(function() {" + js + "})();");
       }
       catch (error) {
         response = "[Broken syntax]";
-      }
-      output.log("commands.js", response);  
+        output.err("commands.js", "Error: " + error);
+      } 
       output.log("commands.js", "JS METHOD CALL ENDED");
       cmdpr._respond(ircdata, response);
     }
@@ -131,7 +149,8 @@ var commands = {
     "version": "version - gives you version and my gist link", 
     "isowner": "isowner - check whether or not you're the owner",
     "stm": "stm <nick> <message> - send a message to make xsbot say it out",
-    "crypt": "crypt <method=otp,caesar,rot13> <key/shift> <plaintext> - Encrypt text"
+    "crypt": "crypt <method=otp,caesar,rot13> <key/shift> <plaintext> - Encrypt text",
+    "quote": "quote - Returns a random quote from my database"
   },
 };
 module.exports = commands;
