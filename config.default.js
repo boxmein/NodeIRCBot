166b4186b76bb6e6cc6cdfd2d51c4fe4691be9dd
boxmein <johannes12321@windowslive.com>
2013-03-21 03:02:35 +0200

    modular also needs 'loud' in its configuration

--- config.default.js
+++ config.default.js
@@ -1,19 +1,19 @@
 
 var config = {
-	verify: function() { return true},
   // For prefix, see commands
   silent: false, // Hides log and channel messages, leaves errors
-  partmsg: "", // Message to send when leaving channel
-  quitmsg: "",  // Message to send when quitting
-  nickname: "", // Bot nickname
-  pass: "", // NickServ password
-  realname: "", // Reported real name
-  channels: "", // Channels separated by comma. Example: #chat,#freenode,#a,#b,#oneliners
-  owner: "" // Use a part of the hostmask
+  loud:  false,  // Used by modular to output errors into the IRC
+  partmsg: "",   // Message to send when leaving channel
+  quitmsg: "",   // Message to send when quitting
+  nickname: "",  // Bot nickname
+  pass: "",      // NickServ password
+  realname: "",  // Reported real name
+  channels: "",  // Channels separated by comma. Example: #chat,#freenode,#a,#b,#oneliners
+  owner: ""      // Use a part of the hostmask
   server: "irc.freenode.net" // IRC server to connect to (irc.freenode.net)
   ownerEmail: "", // Owner of the bot, for user-agents in requests. 
   lastFM: {
-    apiKey: "", // Last.fm API key for commands.lastfm
+    apiKey: "",   // Last.fm API key for commands.lastfm
     secretKey: "" // Last.fm secret key
   }
 };
