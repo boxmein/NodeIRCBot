867f9d7f72bb5a6a3a10555c58925e613452c55b
boxmein <johannes12321@windowslive.com>
2013-03-30 19:43:45 +0200

    Added support for subcommand help text

--- modules/help.js
+++ modules/help.js
@@ -10,14 +10,28 @@ module.exports = {
 		console.log("Initialised help.js");
 	},
 	exec: function(ircdata) {
-		ircdata.args[0] = _.commands.cleanup_query(ircdata.args[0]);
+		var subcmds = ircdata.args[0].split('.');
+		subcmds.map(_.commands.cleanup_query);
 		if (!ircdata.args[0]) {
 			// Run myself with "help" when nothing was used
 			ircdata.args[0] = "help";
 			return this.exec(ircdata);
 		}
 		else if(this.helps.hasOwnProperty(ircdata.args[0])) {
+			if (ircdata.args[0] == "owner" || ircdata.args[0] == "admin")
+				_.commands.respond(ircdata, this.helps[ircdata.args[0]].base);
 			_.commands.respond(ircdata, this.helps[ircdata.args[0]]);
+		}
+		// Strange support for sub-commanded helps.
+		// 1. getHelp() must return an object where base is the helptext
+		// 2. and the object must also have a cmds[] array where every 
+		//    subcommand is its own key/value pair
+		else if(ircdata.args[0].indexOf('.') != -1) {
+			
+			if (this.helps[subcmds[0]].cmds.hasOwnProperty(subcmds[1])) {
+				_.commands.respond(ircdata,this.helps[subcmds[0]].cmds[subcmds[1]]);
+			}
+			else throw "No help found for " + ircdata.args[0];
 		}
 		else throw "No help found for " + ircdata.args[0];
 		
