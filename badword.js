af5f87d47334e0b5566001a4ece039a5ac3d94d4
boxmein <johannes12321@windowslive.com>
2013-04-16 21:41:25 +0300

    Rewrote the badword loop (idunno)

--- badword.js
+++ badword.js
@@ -5,12 +5,15 @@
 var _ = {};
 module.exports = {
 	scan: function (ircdata) {
+		//_.output.log("badword.scan", "Doing scan on badwords...");
 		var msg = ircdata.message.split();
-		_.config.badwords.words.map(function(each) {
-			if (msg == each)
-				return each;
-		});
-		return false;
+		return _.config.badwords.words.map(function(each) {
+			if (msg == each) {
+				_.output.log("badword.scan","Found badword: " + each);
+				return ""+each;
+			}
+		}) || false;
+		
 	},
 	announce: function (ircdata, word) {
 		_.output.log("badword.announce", "Found badword in "+ircdata.channel+"/"+ircdata.sender+", he said " + word);
