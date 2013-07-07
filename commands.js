9f7e4b61d180d9e5123088c206a19466dc39bb11
boxmein <johannes12321@windowslive.com>
2013-07-07 22:02:02 +0300

    Added a modules array / .getCommands for listing

--- commands.js
+++ commands.js
@@ -1,13 +1,37 @@
 var Cmd = require('./mod/Command.js');
 module.exports.cmds = {};
 var _ = null;
+
+var new_commands = [
+  "./mod/hello.js",
+  "./mod/eat.js",
+  "./mod/time.js",
+  "./mod/help.js",
+  "./mod/list.js",
+  "./mod/sendtoxsbot.js"
+  // Add more here,
+  // Don't forget the commas and
+  // No trailing comma!
+  // [a, b, c,] BAD
+  // [a b c] BAD
+  // [a, b, c] GOOD
+];
+
+
+
+// called from entry point --> returned object will become _.helps.
 module.exports.initAll = function(un) {
   _ = un;
-  _.output.log(10, "Starting module loading..."); 
-  // Add your module files here. 
-  // Old modules with loadOld (modules that were for old NodeIRCBot)
-  // New modules with loadNew
-  loadNew("./mod/hello.js"); 
+  _.output.log(0, "Starting module loading..."); 
+  new_commands.map(
+    function(each) { 
+      try {
+        loadNew(each);
+      } catch (err) {
+        _.output.log(20, each + " failed to load! \n" + err + "\n" + err.stack); 
+      }
+    }
+  );
 };
 
 
@@ -27,12 +51,28 @@ var loadOld = function(filename, name) {
   cmd_old.help = cmd_old.helptext; 
   cmd_old.onInit = cmd_old.init; 
   cmd_old.onQuit = cmd_old.die; 
-  cmd_old.fname = name; 
-  
-  _.commands = {};
+  cmd_old.fname = String(name); 
   _.commands.respond = _.respondToSender;
 
   var cmd = new Cmd.Command(cmd_old); 
   module.exports.cmds[cmd.fname] = cmd; 
   cmd.onInit(_); 
+};
+
+// this is for the sake of showing off Module vs command
+module.exports.getCommands = function () {
+  var temp = []; 
+  for (var k in module.exports.cmds) 
+  {
+    if (module.exports.cmds[k].isModule)
+      temp.push(k.toProperCase()); // defined below
+    else temp.push(k);
+  }
+  return temp;
+}
+
+String.prototype.toProperCase = String.prototype.toProperCase || function () {
+    return this.replace(/\w\S*/g, function(txt){
+      return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+    });
 };
